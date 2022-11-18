e842689ebd573ccbd4eed74a99ce00e09b46ff94
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-11-18 13:21:28 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+
+function App() {    
+    return (
+        <div>
+            <Header />
+            <h1>Home Page</h1>
+        </div>
+    )
+}
+
+export default App
