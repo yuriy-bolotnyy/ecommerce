81475522723aae6541caa701e5c45fecadf080d7
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-11-19 18:56:36 -0800

    Add Context Provider

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,10 +1,14 @@
-import React from "react"
+import React, {Component} from "react"
 
 const {Provider, Consumer} = React.createContext()
 
 const ContextProvider = ({children}) => {
+    const [allPhotos, setAllPhotos] = React.useState([])
+
+    const updatePhotos = (photos) => setAllPhotos(photos)
+
     return (
-        <Provider value="">
+        <Provider value={{allPhotos, updatePhotos}}>
             {children}
         </Provider>
     )
