b2325ad6d34316d83abbaa5be9f95d77ff4cdd95
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-11-21 06:12:52 -0800

    Fetch data to Context state

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,11 +1,21 @@
-import React, {Component} from "react"
+import React, {Component, useEffect, useState} from "react"
 
 const {Provider, Consumer} = React.createContext()
 
 const ContextProvider = ({children}) => {
-    const [allPhotos, setAllPhotos] = React.useState([])
+    const [allPhotos, setAllPhotos] = useState([])
 
     const updatePhotos = (photos) => setAllPhotos(photos)
+
+    useEffect(() => {
+        fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
+            .then(response => response.json())
+            .then(data => setAllPhotos(data))
+    }, []);
+
+    useEffect(() => {
+        console.log(allPhotos)
+    }, [allPhotos])
 
     return (
         <Provider value={{allPhotos, updatePhotos}}>
