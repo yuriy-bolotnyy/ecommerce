08edca5f907297cb42eea6f8a4b79e9f862b688d
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-11-22 00:42:19 -0800

    Map context json array to images

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -10,10 +10,21 @@ function Photos() {
             <h1>Images go here</h1>
             <ContextConsumer>
                 {context => {
-                    // console.log(`Consumed Context: ${JSON.stringify(context.allPhotos)}`)
-                    console.log(JSON.stringify(context.allPhotos[0]))
+                    console.log(`Consumed Context: ${JSON.stringify(context.allPhotos)}`)
+                    // console.log(JSON.stringify(context.allPhotos[0]))
+
+                    // context.allPhotos.forEach(element => {
+                    //     console.log(element)
+                    // });
+
+                    const allImages = context.allPhotos.map(el => (
+                        <img src={`${el.url}`} alt={`${el.id}`} className="image-grid"  key={`${el.id}`}/>
+                    ))
+
+                    // console.log(allImages)
+
                     return (
-                        <div></div>
+                        <div>{allImages}</div>
                     )
                 }}
             </ContextConsumer>
