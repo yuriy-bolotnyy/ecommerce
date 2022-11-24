8b84658a965dd708b6bd7d4cf30d86dccef03bc9
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-11-23 17:28:49 -0800

    Add Star and Favorite icons

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -4,6 +4,9 @@ export default function Image({img, className}) {
 
     const [hovered, setHovered] = useState(false)
 
+    const favoriteIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const heartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+
     const mouseEnter = () => {
         setHovered(true)
     }
@@ -17,6 +20,8 @@ export default function Image({img, className}) {
     return (
         <div className={`${className} image-container`}>
             <img src={img.url} alt={img.url} className="image-grid" onMouseOver={mouseEnter} onMouseLeave={mouseLeave}/>
+            {favoriteIcon}
+            {heartIcon}
         </div>
     )
 }
