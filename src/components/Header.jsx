d1a61ab558f3668d259f410cf8b20d56cc264bfc
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-11-26 15:59:12 -0800

    Fill cart icon if any item inside

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
-import React from "react"
+import React, {useContext} from "react"
 import {Link} from "react-router-dom"
+import { Context } from "../Context"
 
 function Header() {
+    const {cartItems} = useContext(Context)
+
+    // Full class name to use WHEN ITEMS IN CART:
+    // "ri-shopping-cart-fill ri-fw ri-2x"
+    // Full class name to use WHEN CART IS EMPTY:
+    // "ri-shopping-cart-line ri-fw ri-2x"
+    const cartBaseClass = cartItems.length > 0 ? 'ri-shopping-cart-fill' : 'ri-shopping-cart-line'
+
     return (
             <header>
                 <Link to="/">
@@ -9,7 +18,7 @@ function Header() {
                 </Link>
                 
                 <Link to="/cart">
-                    <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+                    <i className={`${cartBaseClass} ri-fw ri-2x`}></i>
                 </Link>
                 
             </header>
