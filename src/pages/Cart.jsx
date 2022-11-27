d951c5880eb4f132b8edd77ceee32cf7bbc2f0a0
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-11-26 16:28:49 -0800

    Calculate Total

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,12 +7,15 @@ function Cart() {
     const cartItemElements = cartItems.map(item => (
         <CartItem key={item.id} item={item} />
     ))
-    
+
+    const itemPrice = 5;
+    // const total = cartItems.reduce((accumulator, it) => accumulator + it.price, 0)
+    const total = cartItems.length * itemPrice
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">Total: {total}</p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>
