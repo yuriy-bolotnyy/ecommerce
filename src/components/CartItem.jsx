8660b5fb7917a93e501cc7abb30548422b53f843
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-11-26 16:16:08 -0800

    Click trash btn to remove from cart

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,11 @@
-import React from "react"
+import React, {useContext} from "react"
+import { Context } from "../Context"
 
 function CartItem({item}) {
+    const {removeImageFromCart} = useContext(Context)
     return (
         <div className="cart-item">
-            <i className="ri-delete-bin-line"></i>
+            <i className="ri-delete-bin-line" onClick={() => removeImageFromCart(item)}></i>
             <img src={item.url} width="130px" />
             <p>$5.99</p>
         </div>
